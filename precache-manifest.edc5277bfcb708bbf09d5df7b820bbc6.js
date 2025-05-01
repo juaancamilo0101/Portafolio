ea32b0d1580fe6da9085ac2b146d998c486871bd
Juan Camilo Peña Marin <juaancamilo0101@outlook.com>
2025-05-01 12:21:07 +0200

    Updates

--- precache-manifest.edc5277bfcb708bbf09d5df7b820bbc6.js
+++ precache-manifest.edc5277bfcb708bbf09d5df7b820bbc6.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4cbda6aac450bc075f2b9ca9d438e3c8",
+    "revision": "54222473312fef1ba7dfc37a67c31f16",
     "url": "/Portafolio/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Portafolio/static/css/2.fd277b09.chunk.css"
   },
   {
-    "revision": "38158a527019dc642738",
-    "url": "/Portafolio/static/css/main.76839ea5.chunk.css"
+    "revision": "6b2bf7113093be07ad95",
+    "url": "/Portafolio/static/css/main.85813402.chunk.css"
   },
   {
     "revision": "296c9818f31f973aff36",
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Portafolio/static/js/2.039ebd94.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "38158a527019dc642738",
+    "revision": "6b2bf7113093be07ad95",
     "url": "/Portafolio/static/js/main.5cfd56ed.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portafolio/precache-manifest.be9ccf1c6f4ff14c2d7b37855f2b63d1.js"
+  "/Portafolio/precache-manifest.edc5277bfcb708bbf09d5df7b820bbc6.js"
 );
 
 self.addEventListener('message', (event) => {
